5b7d5326f4d2c60a2977490431ec503d4e96e1b7
Paul Ferguson Maxson <paul.f.maxson@gmail.com>
2020-05-25 17:14:10 -0400

    built and tested midi input adaptor

---
+++ src/machines/midiInputAdaptorCallback/index.test.ts
@@ -0,0 +1,139 @@
+const midi = require('midi');
+
+import makeMidiInputAdaptor, {
+  MidiInput,
+} from './externalClock';
+
+import {
+  Machine,
+  interpret,
+  Actor,
+  assign,
+  spawn,
+} from 'xstate';
+
+const makeConnectedPorts = () => {
+  const input = new midi.Input();
+  input.ignoreTypes(true, false, true);
+  input.openVirtualPort('my-test-port');
+
+  const output = new midi.Output();
+
+  let found = false;
+
+  for (let i = 0; i < output.getPortCount(); i++) {
+    if (output.getPortName(i) === 'my-test-port') {
+      output.openPort(i);
+      found = true;
+    }
+  }
+  if (!found) throw new Error('port not found');
+
+  return [input, output];
+};
+
+interface LocalContext {
+  midiInputAdaptor: Actor;
+  midiInput: MidiInput;
+}
+
+type LocalEvent =
+  | { type: 'PULSE' }
+  | { type: 'STOP' }
+  | { type: 'START' }
+  | { type: 'CONTINUE' }
+  | { type: 'MIDI_INPUT_READY' }
+  | { type: 'MIDI_INPUT_ERROR'; data: Error };
+
+const machine = Machine<LocalContext, LocalEvent>({
+  id: 'test',
+  entry: 'spawnMidiClockInput',
+  initial: 'active',
+  states: {
+    active: {
+      on: {
+        PULSE: { actions: 'spy' },
+        START: { actions: 'spy' },
+        STOP: { actions: 'spy' },
+        CONTINUE: { actions: 'spy' },
+        MIDI_INPUT_ERROR: { actions: 'spy' },
+      },
+    },
+  },
+});
+
+const midiStartMessage = [250];
+const midiStopMessage = [252];
+const midiContinueMessage = [251];
+const midiPulseMessage = [248];
+
+const spawnMidiClockInput = assign<LocalContext, LocalEvent>({
+  midiInputAdaptor: (ctx) =>
+    spawn(
+      makeMidiInputAdaptor<LocalEvent>(
+        ctx.midiInput,
+        new Map([
+          [
+            midiStartMessage[0],
+            () => ({ type: 'START' } as LocalEvent),
+          ],
+          [
+            midiStopMessage[0],
+            () => ({ type: 'STOP' } as LocalEvent),
+          ],
+          [
+            midiContinueMessage[0],
+            () => ({ type: 'CONTINUE' } as LocalEvent),
+          ],
+          [
+            midiPulseMessage[0],
+            () => ({ type: 'PULSE' } as LocalEvent),
+          ],
+        ]),
+        (e) => ({ type: 'MIDI_INPUT_ERROR', data: e }),
+        { type: 'MIDI_INPUT_READY' }
+      ),
+      'midi-clock-input'
+    ),
+});
+
+it('Adapts midi messages to events', (done) => {
+  const [input, output] = makeConnectedPorts();
+
+  const eventSpy = jest.fn();
+
+  const service = interpret(
+    machine.withConfig(
+      {
+        actions: {
+          spawnMidiClockInput,
+          spy: (_, e) => {
+            eventSpy(e);
+          },
+        },
+      },
+      {
+        midiInputAdaptor: undefined,
+        midiInput: input,
+      }
+    )
+  );
+
+  expect.assertions(1);
+
+  setTimeout(() => {
+    try {
+      expect(eventSpy).toHaveBeenCalledWith({ type: 'PULSE' });
+    } catch (e) {
+      done(e);
+    }
+
+    service.stop();
+    output.closePort();
+    input.closePort();
+    done();
+  }, 100);
+
+  output.sendMessage(midiPulseMessage);
+  service.start();
+});
--- src/machines/midiInputAdaptorCallback/index.ts
+++ src/machines/midiInputAdaptorCallback/index.ts
@@ -19,16 +19,20 @@ type MidiMessageMap<TEvent> = Map<
   (midiMessage: MidiMessage, deltaTime: number) => TEvent
 >;
 
+// NOTE: Uses a Map for the midi messages over a Record because its keys will be numbers and I want to avoid confusion with an array.
+
 /** HOF that produces a spawnable callback. The callback reports MIDI messages to its parent.
  * @description The callback sends creates and sends events as specified by the midiMessageMap in response to Midi messages. If an error occurs while the midi subscription is being set up, the actor sends the provided event to the parent.
- * @param midiMessageMap A mapping between Midi event codes and function that builds an event for the callback to send to its parent when a given midi message is received.
- * @param eventOnError A function that generates the event for the Actor to send to its parent when the subscription process throws an error.
  * @param midiInput The midi input object. While this actor was designed to work with node-midi out of the box, any midi library can be used as long as it fits the subscription pattern.
+ * @param midiMessageMap A mapping between Midi event codes and function that builds an event for the callback to send to its parent when a given midi message is received.
+ * @param makeOnError A function that generates the event for the Actor to send to its parent when the subscription process throws an error.
+ * @param onReady The event for the Actor to send to its parent when the subscription is succesfully completed.
  * @returns An invokable or spawnable callback
  *
  * @example
  * assign({
  *  midiInputAdaptor: (ctx) => spawn(makeMidiInputAdaptor<LocalEvent>(
+ *    ctx.midiInput,
  *    new Map([
  *      [250, () => ({type: 'START'})],
  *      [252, () => ({type: 'STOP'})],
@@ -36,14 +40,17 @@ type MidiMessageMap<TEvent> = Map<
  *      [248, () => ({type: 'PULSE'})],
  *    ]),
  *    (e) => {type: 'MIDI_INPUT_ERROR', data: e},
- *    ctx.midiInput
+ *    {type: 'CLOCK_INPUT_READY'}
  *  ), 'midi-clock-input')
  * })
  */
 const makeMidiInputAdaptor = <TEvent extends AnyEventObject>(
-  midiMessageMap: MidiMessageMap<TEvent>,
-  eventOnError: (e: Error) => TEvent,
-  midiInput: MidiInput
+  midiInput: MidiInput,
+  midiMessageMap: MidiMessageMap<TEvent> = new Map(),
+  makeOnError: (e: Error) => TEvent = (e) => {
+    throw e;
+  },
+  onReady?: TEvent
 ) =>
   ((sendParent) => {
     try {
@@ -61,8 +68,10 @@ const makeMidiInputAdaptor = <TEvent extends AnyEventObject>(
         }
       );
     } catch (error) {
-      sendParent(eventOnError);
+      sendParent(makeOnError(error));
     }
+
+    sendParent(onReady);
   }) as InvokeCallback;
 
 export default makeMidiInputAdaptor;
